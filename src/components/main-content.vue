6970d27d2460b9730839ce3452d08a4950188aba
Anfal <muhammadanfal0@gmail.com>
2020-10-10 15:14:44 -0700

    WIP

--- src/components/main-content.vue
+++ src/components/main-content.vue
@@ -1,4 +1,8 @@
 <template>
-  <div>
+  <div class="container-fluid mt-4">
+    <div class="row">
+      <div class="col-md-6">cd</div>
+      <div class="col-md-6">dcsc</div>
+    </div>
   </div>
 </template>
