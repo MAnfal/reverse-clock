d2326aa0e498b2b16750c3109e1504f9ab8f630e
Anfal <muhammadanfal0@gmail.com>
2020-10-11 04:49:20 -0700

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
     <Header />
+
     <main-content />
+
+    <Footer />
   </div>
 </template>
 
 <script>
 import MainContent from './components/main-content.vue';
 import Header from './components/header.vue';
+import Footer from './components/footer.vue';
 
 export default {
   name: 'App',
   components: {
     MainContent,
     Header,
+    Footer
   }
 }
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer bottom-footer">
+      <div class="container">
+        <span class="text-muted">
+            <i class="fas fa-copyright"></i> {{ new Date().getFullYear() }} Made with <i class="fas fa-heart" style="color: #eb4b60;"></i> by <a href="https://linktr.ee/manfal" target="_blank">Anfal</a>
+        </span>
+      </div>
+    </footer>
+</template>
+
+<style scoped>
+    .bottom-footer {
+        position: absolute;
+        width: 100%;
+        padding: 1%;
+        text-align: center;
+        bottom: 0;
+        border-top: 1px solid;
+    }
+</style>
