854a052554ad80ca309d2b91a0ec684c8a9eaa13
Anfal <muhammadanfal0@gmail.com>
2020-10-11 19:50:40 -0700

    Remove hamburger icon

--- src/components/header.vue
+++ src/components/header.vue
@@ -5,9 +5,9 @@
         {{ tagLine }}
     </a>
     
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>
+    </button> -->
 
     <!-- <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
